4ccad8627710c387006e75fac5795fc053a91d1e
Federico <fedeaacevedo@gmail.com>
2022-07-26 18:22:03 -0300

    Agregar archivos de proyecto.

---
+++ MVCCore.MongoDB.CRUD/Models/Album.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCore.MongoDB.CRUD.Models
+{
+    public class Album
+    {
+        [BsonId]
+        public int ObjectId { get; set; }
+        public string AlbumName { get; set; }
+        public string Artist { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Duration { get; set; }
+
+    }
+}
---
+++ MVCCore.MongoDB.CRUD/Repositories/MongoDBRepository.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCore.MongoDB.CRUD.Repositories
+{
+    public class MongoDBRepository
+    {
+        public MongoClient client;
+        public IMongoDatabase db;
+
+        public MongoDBRepository()
+        {
+            client = new MongoClient("mongodb://localhost:27017");
+
+            db = client.GetDatabase("MusicCatalog");
+        }
+    }
+}

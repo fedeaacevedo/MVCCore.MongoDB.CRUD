2cb5375b567a4751c2335fcd06a5aeb0223dc509
Federico <fedeaacevedo@gmail.com>
2022-07-26 18:32:35 -0300

    AlgumCollection

---
+++ MVCCore.MongoDB.CRUD/Repositories/AlbumCollection.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Driver;
+using MVCCore.MongoDB.CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCore.MongoDB.CRUD.Repositories
+{
+    public class AlbumCollection : IAlbumCollection
+    {
+        internal MongoDBRepository _repository = new MongoDBRepository();
+        private IMongoCollection<Album> Collection;
+    
+        public AlbumCollection()
+        {
+            Collection = _repository.db.GetCollection<Album>("Albums");
+        }
+
+        public void DeleteAlbum(Album album)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Album GetAlbumById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Album> GetAllAlbums()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertAlbum(Album album)
+        {
+            Collection.InsertOneAsync(album);
+        }
+
+        public void UpdateAlbum(Album album)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
